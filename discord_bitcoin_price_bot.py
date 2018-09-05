d2d40ad815043871f0d50cfbf775b1cf22a0254b
DelpHi4 <43003119+DelpHi4@users.noreply.github.com>
2018-09-05 16:51:21 +0300

    Update discord_bitcoin_price_bot.py

--- discord_bitcoin_price_bot.py
+++ discord_bitcoin_price_bot.py
@@ -1,23 +1,23 @@
 import random
 import asyncio
-import requests
-from discord import Game
-from discord.ext.commands import Bot
 import aiohttp
 import json
+from discord import Game
+from discord.ext.commands import Bot
+
 BOT_PREFIX = ("?", "!")
-TOKEN = "NDg2NTM4MjcyNzI2NTgxMjQ4.DnFvsw.BZMxB01L-W0T7-448CndgSCOSww"
 
 client = Bot(command_prefix=BOT_PREFIX)
 
-
-
-@client.command(pass_context=True)
-async def bitcoin(ctx):
+@client.command()
+async def bitcoin():
     url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
-    response = requests.get(url)
-    value = response.json()['bpi']['USD']['rate']
-    await client.send_message(ctx.message.channel, "Bitcoin price is: $" + value)
+    async with aiohttp.ClientSession() as session:  # Async HTTP request
+        raw_response = await session.get(url)
+        response = await raw_response.text()
+        response = json.loads(response)
+        await client.say("Bitcoin price is: $" + response['bpi']['USD']['rate'])
+
 @client.command()
 async def eth():
     url = 'https://api.coinmarketcap.com/v1/ticker/ethereum/'
@@ -58,82 +58,83 @@ async def ripple():
     await client.say("Ripples percent change in the past 7 days is:" + change72)
     await client.say("Ripples 24 hour volume is:" + vol24)
     await client.say("Ripples symbol is: " + sym)
+    
 @client.command()
 async def litecoin():
     url = 'https://api.coinmarketcap.com/v1/ticker/litecoin/'
     response = requests.get(url)
     value = response.json()[0]["price_usd"]
     await client.say("Litecoin price is: $" + value)
+    
 @client.command()
 async def neo():
     url = 'https://api.coinmarketcap.com/v1/ticker/neo/'
     response = requests.get(url)
     value = response.json()[0]["price_usd"]
     await client.say("NEO price is: $" + value)
+    
 @client.command()
 async def eos():
     url = 'https://api.coinmarketcap.com/v1/ticker/eos/'
     response = requests.get(url)
     value = response.json()[0]["price_usd"]
     await client.say("EOS price is: $" + value)
+    
 @client.command()
 async def dash():
     url = 'https://api.coinmarketcap.com/v1/ticker/dash/'
     response = requests.get(url)
     value = response.json()[0]["price_usd"]
     await client.say("DASH price is: $" + value)
+    
 @client.command()
 async def zcash():
     url = 'https://api.coinmarketcap.com/v1/ticker/zcash/'
     response = requests.get(url)
     value = response.json()[0]["price_usd"]
     await client.say("Zcash price is: $" + value)
+    
 @client.command()
 async def metal():
     url = 'https://api.coinmarketcap.com/v1/ticker/metal/'
     response = requests.get(url)
     value = response.json()[0]["price_usd"]
     await client.say("METAL coin price is: $" + value)
+    
 @client.command()
 async def trump():
     url = 'https://api.coinmarketcap.com/v1/ticker/trumpcoin/'
     response = requests.get(url)
     value = response.json()[0]["price_usd"]
     await client.say("Trump Coin price is: $" + value)
+    
 @client.command()
 async def gas():
     url = 'https://api.coinmarketcap.com/v1/ticker/gas/'
     response = requests.get(url)
     value = response.json()[0]["price_usd"]
     await client.say("GAS coin price is: $" + value)
+    
 @client.command()
 async def gts():
     url = 'https://api.coinmarketcap.com/v1/ticker/game/'
     response = requests.get(url)
     value = response.json()[0]["price_usd"]
     await client.say("GTS coin price is: $" + value)
+    
 @client.command()
 async def salus():
     url = 'https://api.coinmarketcap.com/v1/ticker/salus/'
     response = requests.get(url)
     value = response.json()[0]["price_usd"]
-    await client.say("Salus coin price is: $" + value)
-
-##########################
-
-
-
-
-
+    await client.say("Salus coin price is: $" + value)  
+    
+    
+    
+        
+            
 
 
 
 
-
-
-
-
-
-
-
-client.run("NDg2NTM4MjcyNzI2NTgxMjQ4.DnFvsw.BZMxB01L-W0T7-448CndgSCOSww")
+client.run("NDg1NTAzMzQ5NTE3NzEzNDE4.Dm0jJw.RjBFbnkIQEcPE-zpP6EHTyquzoQ")
