393b126a5018605080dc4c9ad69c7ae864580bac
DelpHi4 <43003119+DelpHi4@users.noreply.github.com>
2018-09-05 17:03:18 +0300

    Update discord_bitcoin_price_bot.py

--- discord_bitcoin_price_bot.py
+++ discord_bitcoin_price_bot.py
@@ -17,8 +17,9 @@ async def bitcoin():
         response = await raw_response.text()
         response = json.loads(response)
         await client.say("Bitcoin price is: $" + response['bpi']['USD']['rate'])
+        
 
-@client.command()
+
 async def eth():
     url = 'https://api.coinmarketcap.com/v1/ticker/ethereum/'
     response = requests.get(url)
@@ -38,7 +39,7 @@ async def eth():
     await client.say("Ethereums percent change in the past 7 days is: " + change72)
     await client.say("Ethereums 24 hour volume is: " + vol24)
     await client.say("Ethereums symbol is: " + sym)
-@client.command()
+
 async def ripple():
     url = 'https://api.coinmarketcap.com/v1/ticker/ripple/'
     response = requests.get(url)
@@ -59,70 +60,70 @@ async def ripple():
     await client.say("Ripples 24 hour volume is:" + vol24)
     await client.say("Ripples symbol is: " + sym)
     
-@client.command()
+
 async def litecoin():
     url = 'https://api.coinmarketcap.com/v1/ticker/litecoin/'
     response = requests.get(url)
     value = response.json()[0]["price_usd"]
     await client.say("Litecoin price is: $" + value)
     
-@client.command()
+
 async def neo():
     url = 'https://api.coinmarketcap.com/v1/ticker/neo/'
     response = requests.get(url)
     value = response.json()[0]["price_usd"]
     await client.say("NEO price is: $" + value)
     
-@client.command()
+
 async def eos():
     url = 'https://api.coinmarketcap.com/v1/ticker/eos/'
     response = requests.get(url)
     value = response.json()[0]["price_usd"]
     await client.say("EOS price is: $" + value)
     
-@client.command()
+
 async def dash():
     url = 'https://api.coinmarketcap.com/v1/ticker/dash/'
     response = requests.get(url)
     value = response.json()[0]["price_usd"]
     await client.say("DASH price is: $" + value)
     
-@client.command()
+
 async def zcash():
     url = 'https://api.coinmarketcap.com/v1/ticker/zcash/'
     response = requests.get(url)
     value = response.json()[0]["price_usd"]
     await client.say("Zcash price is: $" + value)
+
     
-@client.command()
 async def metal():
     url = 'https://api.coinmarketcap.com/v1/ticker/metal/'
     response = requests.get(url)
     value = response.json()[0]["price_usd"]
     await client.say("METAL coin price is: $" + value)
     
-@client.command()
+
 async def trump():
     url = 'https://api.coinmarketcap.com/v1/ticker/trumpcoin/'
     response = requests.get(url)
     value = response.json()[0]["price_usd"]
     await client.say("Trump Coin price is: $" + value)
     
-@client.command()
+
 async def gas():
     url = 'https://api.coinmarketcap.com/v1/ticker/gas/'
     response = requests.get(url)
     value = response.json()[0]["price_usd"]
     await client.say("GAS coin price is: $" + value)
     
-@client.command()
+
 async def gts():
     url = 'https://api.coinmarketcap.com/v1/ticker/game/'
     response = requests.get(url)
     value = response.json()[0]["price_usd"]
     await client.say("GTS coin price is: $" + value)
     
-@client.command()
+
 async def salus():
     url = 'https://api.coinmarketcap.com/v1/ticker/salus/'
     response = requests.get(url)
