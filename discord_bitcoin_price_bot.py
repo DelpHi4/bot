2096c48141e3bd5df5bbd104f58adc960a0eb477
DelpHi4 <43003119+DelpHi4@users.noreply.github.com>
2018-09-10 20:57:21 +0300

    Update discord_bitcoin_price_bot.py

--- discord_bitcoin_price_bot.py
+++ discord_bitcoin_price_bot.py
@@ -8,6 +8,7 @@ from discord.ext.commands import Bot
 BOT_PREFIX = ("?", "!")
 
 client = Bot(command_prefix=BOT_PREFIX)
+bot = commands.Bot(command_prefix='?', '!')
 
 @client.command()
 async def bitcoin():
